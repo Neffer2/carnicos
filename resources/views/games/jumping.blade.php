06855beb7c0b82e92f2ac953f7ab6d5afbff910a
N3f73r <n3f73r@gmail.com>
2022-09-13 22:38:15 -0500

    jumping grilla

--- resources/views/games/jumping.blade.php
+++ resources/views/games/jumping.blade.php
@@ -9,10 +9,18 @@
 
     @section('content')
         <div class="container">
-            <div class="game-item left-side">1</div>
-            <div id="game" class="game-item game"></div>
-            <div class="game-item right-side">3</div>
-        </div>  
+            <div class="left-side">
+                <div class="left-side-item">No te conviene que yo te recuerde lo de jimenez</div>
+                <div class="left-side-item">2</div>
+                <div class="left-side-item">3</div>
+            </div>
+            <div id="game" class="game"></div>
+            <div class="right-side">
+                <div class="right-side-item">1</div>
+                <div class="right-side-item">2</div>
+                <div class="right-side-item">Tas viviendo alquilado y los carros son todos del Buddy</div>
+            </div>
+        </div>   
     @endsection
 
     @section('scripts')
